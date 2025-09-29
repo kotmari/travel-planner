b34bd04338faf710fb2f3b506dddd5e1b1e1b77c
kotmari <mariannakot79@gmail.com>
2025-09-29 16:07:10 +0300

    update oage globe

---
+++ next.config.ts
@@ -0,0 +1,12 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  images: {
+    remotePatterns: [{
+      hostname: "v6sbyijyy0.ufs.sh"
+    }]
+  }
+};
+
+export default nextConfig;
