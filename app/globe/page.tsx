49a0d978420a46f97f5e62f08f7d5a8ef2fab869
kotmari <mariannakot79@gmail.com>
2025-09-29 16:18:11 +0300

    update package.json

--- app/globe/page.tsx
+++ app/globe/page.tsx
@@ -69,7 +69,7 @@ export default function GlobePage() {
               <div className="p-6">
                 <h2 className="text-2xl font-semibold mb-4">
                   {" "}
-                  See where you've been...
+                  See where you&lsquo;ve been...
                 </h2>
 
                 <div className="h-[600px] w-full relative">
@@ -117,7 +117,7 @@ export default function GlobePage() {
                       <div className="bg-blue-50 p-4 rounded-lg">
                         <p className="text-sm text-blue-800">
                           {" "}
-                          You've visited{" "}
+                          You&lsquo;ve visited{" "}
                           <span className="font-bold">
                             {" "}
                             {visitedCountries.size}
